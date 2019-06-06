f75e4eb2f4d312e9f6b6a767e6b7316a5326d986
shubhamsapra97 <shubham.sapra97@gmail.com>
2019-06-07 02:38:45 +0530

    login component updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,22 +5,6 @@ class Login extends Component {
     
     constructor(props) {
         super(props);
-        this.state = {
-            username: '',
-            password: ''
-        }
-    }
-    
-    onUsernameChange = (e) => {
-        this.setState({
-           username: e.target.value
-        });
-    }
-    
-    onPasswordChange = (e) => {
-        this.setState({
-           password: e.target.value
-        });
     }
     
     render() {
@@ -28,12 +12,8 @@ class Login extends Component {
         
             <form className="box" action="/login" method="POST">
                 <h1>Login</h1>
-                <input type="text" placeholder="Username" name="username" autoComplete="off"
-                     required
-                />
-                <input type="password" placeholder="Password" name="password" autoComplete="off"
-                     required
-                />
+                <input type="text" placeholder="Username" name="username" autoComplete="off" required />
+                <input type="password" placeholder="Password" name="password" autoComplete="off" required />
                 <input type="button" value="Sign Up" name="register" />
                 <input type="submit" value="Login" name="login" />
             </form>
